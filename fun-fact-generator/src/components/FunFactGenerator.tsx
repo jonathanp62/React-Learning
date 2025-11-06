a05620efa66b744671ca384e2dab91e09a830751
Jonathan Parker <jonathanp62@gmail.com>
2025-11-06 06:31:48 -0500

    Fun Fact Generator - Completed

--- fun-fact-generator/src/components/FunFactGenerator.tsx
+++ fun-fact-generator/src/components/FunFactGenerator.tsx
@@ -30,7 +30,19 @@
 
 import type { JSX } from "react";
 
-import { useTranslation } from 'react-i18next';
+import { useState} from "react";
+import { useTranslation } from "react-i18next";
+
+import packageJson from "../../package.json";
+
+type FunFact = {
+    id: string
+    text: string
+    source: string
+    source_url: string
+    language: string
+    permalink: string
+}
 
 /**
  * The fun fact generator component.
@@ -39,10 +51,36 @@ import { useTranslation } from 'react-i18next';
  */
 export default function FunFactGenerator(): JSX.Element {
     const { t } = useTranslation();
+    const [fact, setFact] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchFunFact: () => Promise<void> = async (): Promise<void> => {
+        setLoading(true);
+
+        try {
+            const dataSource: string = packageJson.appConfig.dataSource;
+            const response: Response = await fetch(dataSource);
+            const data: FunFact = await response.json();
+
+            setFact(data.text);
+        } catch (error) {
+            setFact(`Error fetching a fun fact: ${error}`);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="max-w-md p-6 bg-white rounded-lg shadow-lg text-center">
             <h1 className="text-2xl font-bold text-gray-700 mb-4">{ t("title") }</h1>
+            <p className="text-gray-600 italic mb-6">{ fact || `${t("prompt")}!` }</p>
+
+            <button
+                onClick={ fetchFunFact }
+                className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none"
+            >
+                { loading ? `${t("loading")}...` : t("get-fun-fact") }
+            </button>
         </div>
     );
 }
