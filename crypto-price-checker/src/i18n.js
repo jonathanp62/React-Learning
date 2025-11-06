ce45de0361858c7456d44998172b912ed0642164
Jonathan Parker <jonathanp62@gmail.com>
2025-11-06 12:15:25 -0500

    Crypto Price Checker - Initial 0.2.0 commit

---
+++ crypto-price-checker/src/i18n.js
@@ -0,0 +1,58 @@
+/*
+ * (#)i18n.js   0.2.0   11/06/2025
+ *
+ * @author  Jonathan Parker
+ * @version 0.2.0
+ * @since   0.2.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import your translation files.
+import enTranslation from './locales/en/translation.json';
+
+// The translations
+const resources = {
+    en: {
+        translation: enTranslation,
+    },
+};
+
+i18n
+    .use(LanguageDetector) // Detects the user's language.
+    .use(initReactI18next) // Passes the i18n instance to react-i18next.
+    .init({ // Initializes i18next.
+        resources,
+        lng: 'en', // default language
+        fallbackLng: 'en', // fallback language if translation not found
+        interpolation: {
+            escapeValue: false, // React already escapes values
+        },
+        debug: false
+    });
+
+export default i18n;
