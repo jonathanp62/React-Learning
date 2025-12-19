ab9910cf6371b633f739149939d421f01b1c7262
Jonathan Parker <jonathanp62@gmail.com>
2025-12-19 16:04:49 -0500

    E-Commerce - The order detail page is almost done except for some different formatting

--- e-commerce/src/pages/OrderDetail.tsx
+++ e-commerce/src/pages/OrderDetail.tsx
@@ -32,7 +32,7 @@ import type { JSX } from "react";
 import type { OrderDocument } from "../types/OrderDocument";
 import type { Product } from "../types/Product";
 
-import {formatIso8601Date, formatPrice, formatRating} from "../utils/Formatters";
+import { formatIso8601Date, formatPrice, formatRating } from "../utils/Formatters";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
@@ -92,6 +92,12 @@ export default function OrderDetail(): JSX.Element {
         void fetchOrderData();
     }, []);
 
+    const computeTotal: (products: Product[]) => number = (products: Product[]): number => {
+        return products.reduce((total: number, product: Product): number => {
+            return total + product.price;
+        }, 0);
+    };
+
     return (
         <div className="w-full max-w-[1000px] mx-auto pt-4 relative">
             <p className="font-bold text-2xl mb-2 dark:text-white">{ t("order") } { orderId?.toUpperCase() }</p>
@@ -102,13 +108,12 @@ export default function OrderDetail(): JSX.Element {
                 <div className="flex justify-between">
                     <div className="mt-16">
                         {
-                            order.products.map((product: Product, index: number): JSX.Element => (
+                            order.products.map((product: Product): JSX.Element => (
                                 <div className="w-full border-b-2 p-6 mt-3 flex border-black">
                                     <div className="w-full flex justify-between gap-x-10">
                                         <div className="w-[170px] object-fill">
                                             <img src={ product.image } alt={ t("product-image") } className=""/>
                                         </div>
-
                                         <div className="w-[450px] flex flex-col gap-y-4">
                                             <h1 className="font-semibold text-lg dark:text-white">{ product.title }</h1>
                                             <h1 className="text-sm dark:text-white">{ product.description }</h1>
@@ -121,6 +126,46 @@ export default function OrderDetail(): JSX.Element {
                                 </div>
                             ))
                         }
+                    </div>
+                    <div className="mt-32 flex flex-col justify-between h-[500px] ml-8">
+                        <div className="uppercase text-green-700 font-semibold">{ t("your-order") }</div>
+                        <div className="uppercase text-green-700 font-bold text-4xl">{ t("summary") }</div>
+                        <p className="mt-3 font-bold dark:text-white">
+                            <span>{ t("total-items") }: {order.products.length}</span>
+                        </p>
+                        <p className="mt-5 font-bold dark:text-white">
+                            <span>{ t("ordered-by") }</span>
+                        </p>
+                        <p className="mt-1 font-bold dark:text-white">
+                            <span className="font-normal">{order.firstName} {order.lastName} on {formatIso8601Date(order.orderDate)}</span>
+                        </p>
+                        <p className="mt-5 font-bold dark:text-white">
+                            <span>{ t("total") }: {formatPrice(computeTotal(order.products))}</span>
+                        </p>
+                        <p className="mt-5 font-bold dark:text-white">
+                            <span>Shipping Information</span>
+                        </p>
+                        <p className="mt-1 font-bold dark:text-white">
+                            <span className="font-normal">{order.firstName} {order.lastName}</span>
+                        </p>
+                        <p className="mt-0 font-bold dark:text-white">
+                            <span className="font-normal">{order.address}</span>
+                        </p>
+                        <p className="mt-0 font-bold dark:text-white">
+                            <span className="font-normal">{order.city}, {order.state} {order.zipCode}</span>
+                        </p>
+                        <p className="mt-0 font-bold dark:text-white">
+                            <span className="font-normal">{order.country}</span>
+                        </p>
+                        <p className="mt-5 font-bold dark:text-white">
+                            <span>Contact Information</span>
+                        </p>
+                        <p className="mt-1 font-bold dark:text-white">
+                            <span className="font-normal">{order.phone}</span>
+                        </p>
+                        <p className="mt-0 font-bold dark:text-white">
+                            <span className="font-normal">{order.email}</span>
+                        </p>
                     </div>
                 </div>
             ) : (
